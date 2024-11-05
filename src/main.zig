53d90778bc10d8fa66457b9d48bdcc32cf696703
Kristófer R. <kristofer@thorlaksson.com>
2024-11-05 17:15:22 -0500

    editor: handle newlines and tabs

--- src/main.zig
+++ src/main.zig
@@ -7,11 +7,15 @@ const Cursor = struct {
     column: usize,
 };
 
-const Editor = struct {
-    rich_text: vxfw.RichText,
-    children: [1]vxfw.SubSurface = undefined,
+const Line = struct {
     text: std.ArrayList(u8),
+};
+
+const Editor = struct {
     cursor: Cursor,
+    lines: std.ArrayList(Line),
+
+    gpa: std.mem.Allocator,
 
     pub fn widget(self: *Editor) vxfw.Widget {
         return .{
@@ -24,16 +28,29 @@ const Editor = struct {
     pub fn handleEvent(self: *Editor, ctx: *vxfw.EventContext, event: vxfw.Event) anyerror!void {
         switch (event) {
             .key_press => |key| {
-                if (key.text) |t| {
-                    try self.text.appendSlice(t);
+                if (key.matches(vaxis.Key.enter, .{})) {
+                    const line: Line = .{ .text = std.ArrayList(u8).init(self.gpa) };
+                    try self.lines.append(line);
 
+                    self.cursor.line +|= 1;
+                    self.cursor.column = 0;
+
+                    // We need to make sure we redraw the widget after changing the text.
+                    ctx.consumeAndRedraw();
+                } else if (key.matches(vaxis.Key.tab, .{})) {
+                    // FIXME: Handle tabs properly without hard-coding spaces.
+                    try self.lines.items[self.cursor.line].text.appendSlice("    ");
+                    self.cursor.column +|= 4;
+
+                    // We need to make sure we redraw the widget after changing the text.
+                    ctx.consumeAndRedraw();
+                } else if (key.text) |t| {
+                    try self.lines.items[self.cursor.line].text.appendSlice(t);
                     self.cursor.column +|= 1;
 
+                    // We need to make sure we redraw the widget after changing the text.
                     ctx.consumeAndRedraw();
                 }
-            },
-            .focus_in => {
-                return ctx.requestFocus(self.rich_text.widget());
             },
             else => {},
         }
@@ -47,23 +64,30 @@ const Editor = struct {
         //        multiple styled spans. When we get to that point we'll want to only modify
         //        the spans in the line that's been changed, and do this when the key event is
         //        is handled.
-        var spans = std.ArrayList(vxfw.RichText.TextSpan).init(ctx.arena);
-        const span: vxfw.RichText.TextSpan = .{ .text = self.text.items };
-        try spans.append(span);
-        self.rich_text.text = spans.items;
+        const rte_widgets = try ctx.arena.alloc(vxfw.RichText, self.lines.items.len);
+        for (self.lines.items, 0..) |l, i| {
+            var spans = std.ArrayList(vxfw.RichText.TextSpan).init(ctx.arena);
+            const span: vxfw.RichText.TextSpan = .{ .text = l.text.items };
+            try spans.append(span);
 
-        // Draw the RichText widget with the spans.
-        const rich_text_surface = try self.rich_text.widget().draw(ctx);
-        self.children[0] = .{
-            .surface = rich_text_surface,
-            .origin = .{ .row = 0, .col = 0 },
-        };
+            rte_widgets[i] = .{ .text = spans.items };
+        }
+
+        // Draw RichText widgets.
+        const children = try ctx.arena.alloc(vxfw.SubSurface, rte_widgets.len);
+        for (rte_widgets, 0..) |rte, i| {
+            const surface = try rte.widget().draw(ctx);
+            children[i] = .{
+                .surface = surface,
+                .origin = .{ .row = @intCast(i), .col = 0 },
+            };
+        }
 
         return .{
             .size = max,
             .widget = self.widget(),
             .buffer = &.{},
-            .children = &self.children,
+            .children = children,
             .focusable = true,
             .cursor = .{
                 .row = @truncate(self.cursor.line),
@@ -145,15 +169,24 @@ pub fn main() !void {
     const fnApp = try allocator.create(Fn);
     defer allocator.destroy(fnApp);
 
-    const text: vxfw.RichText.TextSpan = .{ .text = "" };
+    // Set up initial state.
+    const line: Line = .{ .text = std.ArrayList(u8).init(allocator) };
+    var lines = std.ArrayList(Line).init(allocator);
+    try lines.append(line);
+
     fnApp.* = .{
         .editor = .{
-            .rich_text = .{ .text = &.{text} },
-            .text = std.ArrayList(u8).init(allocator),
             .cursor = .{ .line = 0, .column = 0 },
+            .lines = lines,
+            .gpa = allocator,
         },
     };
-    defer fnApp.editor.text.deinit();
+    defer {
+        for (fnApp.editor.lines.items) |l| {
+            l.text.deinit();
+        }
+        fnApp.editor.lines.deinit();
+    }
 
     try app.run(fnApp.widget(), .{});
     app.deinit();
