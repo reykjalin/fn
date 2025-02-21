a79f15162f369eca407097af67cfc49c8b277b7c
Kristófer R. <kristofer@thorlaksson.com>
2025-02-21 03:35:34 +0000

    libfn: add eql and strictEql functions to Selection, and tests

--- src/libfn/selection.zig
+++ src/libfn/selection.zig
@@ -32,12 +32,60 @@ pub fn fromRange(range: Range) Selection {
     return .{ .cursor = range.to, .anchor = range.from };
 }
 
+/// Returns `true` if a and b cover the same areas of the text editor. The order of the `.cursor`
+/// and `.anchor` positions within each selection does not matter.
+pub fn eql(a: Selection, b: Selection) bool {
+    return a.toRange().eql(b.toRange());
+}
+
+/// Returns `true` if and only if a.cursor == b.cursor and a.anchor == b.anchor. In other words; the
+/// order of `.anchor` and `.cursor` positions within the selection matters.
+pub fn strictEql(a: Selection, b: Selection) bool {
+    return a.cursor == b.cursor and a.anchor == b.anchor;
+}
+
 test isCursor {
     const empty: Selection = .{ .anchor = Pos.fromInt(1), .cursor = Pos.fromInt(1) };
     const not_empty: Selection = .{ .anchor = Pos.fromInt(1), .cursor = Pos.fromInt(2) };
 
     try std.testing.expectEqual(true, empty.isCursor());
     try std.testing.expectEqual(false, not_empty.isCursor());
+}
+
+test eql {
+    const a: Selection = .{ .anchor = Pos.fromInt(0), .cursor = Pos.fromInt(3) };
+    const b: Selection = .{ .anchor = Pos.fromInt(3), .cursor = Pos.fromInt(0) };
+    const c: Selection = .{ .anchor = Pos.fromInt(1), .cursor = Pos.fromInt(5) };
+
+    try std.testing.expectEqual(true, Selection.eql(a, a));
+    try std.testing.expectEqual(true, Selection.eql(b, b));
+    try std.testing.expectEqual(true, Selection.eql(c, c));
+
+    try std.testing.expectEqual(true, Selection.eql(a, b));
+    try std.testing.expectEqual(true, Selection.eql(b, a));
+
+    try std.testing.expectEqual(false, Selection.eql(c, a));
+    try std.testing.expectEqual(false, Selection.eql(a, c));
+    try std.testing.expectEqual(false, Selection.eql(c, b));
+    try std.testing.expectEqual(false, Selection.eql(b, c));
+}
+
+test strictEql {
+    const a: Selection = .{ .anchor = Pos.fromInt(0), .cursor = Pos.fromInt(3) };
+    const b: Selection = .{ .anchor = Pos.fromInt(3), .cursor = Pos.fromInt(0) };
+    const c: Selection = .{ .anchor = Pos.fromInt(1), .cursor = Pos.fromInt(5) };
+
+    try std.testing.expectEqual(true, Selection.strictEql(a, a));
+    try std.testing.expectEqual(true, Selection.strictEql(b, b));
+    try std.testing.expectEqual(true, Selection.strictEql(c, c));
+
+    try std.testing.expectEqual(false, Selection.strictEql(a, b));
+    try std.testing.expectEqual(false, Selection.strictEql(b, a));
+
+    try std.testing.expectEqual(false, Selection.strictEql(c, a));
+    try std.testing.expectEqual(false, Selection.strictEql(a, c));
+    try std.testing.expectEqual(false, Selection.strictEql(c, b));
+    try std.testing.expectEqual(false, Selection.strictEql(b, c));
 }
 
 test "refAllDecls" {
